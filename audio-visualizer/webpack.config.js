4973394c7c8389ee0debec073cff328d72589e48
yaboiedchoi <ec2244@rit.edu>
2025-04-14 06:23:06 -0400

    first successful docker setup!
    includes webpack and babel, and will automatically build and transpile and bundle the ES6 modules down to ES5

--- audio-visualizer/webpack.config.js
+++ audio-visualizer/webpack.config.js
@@ -1,7 +1,17 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    mode: 'production',
-    entry: ['./src/loader.js', './src/navbar-menu.js'],
-    output: {
-      filename: './bundle.js'
-    }
-  };
+  entry: './src/loader.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // optional: clears old files
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+  ],
+  mode: 'production',
+};
